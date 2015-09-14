1d6db3081dd5da6a504e3d8b9d4320f8a01e3973
Gaurav Sheni <gvsheni@gmail.com>
2015-09-14 17:09:21 -0400

    To parralize the program it needs to used threads. So the kernel call function had to be rewritten.

--- DecodeEmail2FULP.cu
+++ DecodeEmail2FULP.cu
@@ -1,53 +1,69 @@
+// Gaurav Sheni
+// CSC 391 
+// September 16, 2015
+// Project 1
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-__global__ void convert2nums(char* line, char answer[]);
+//declaring kernel call
+__global__ void decrement(char* line, char* answer);
 
-__global__ void convert2nums(char* line, char answer[]){
-	int i;
-	int current;
-	char character;
-
-	for( i = 0; line[i] != '\0'; i++) {
-		current = (int) line[i];
-		character = (char) (current -1);
-		answer[i] = character;
-	}
+__global__ void decrement(char *current, char* answer){
+	int i = threadIdx.x;
+	answer[i] = (char)( (int) current[i] - 1 );
 }
 
 int main ( int argc, char *argv[] )
 {
-		if (argc != 2){
+	//check to make sure that there is only two command line arguments
+	if (argc != 2){
 		printf ("Incorrect number of command line arugments.\r\n");
-		exit(0);
+		//exit with 1 because exit(1) indicates that there were an error
+		exit(1);
 	}
 
-	FILE *file = fopen(argv[1], "r"); // "r" for read
+	//open the file to be read, give the first, argument
+	//"r" is for read
+	FILE *file = fopen(argv[1], "r"); 
 
+	//line keeps the input data, inialize to null. 
 	char* line = NULL;
+	//for determining how many characters are in the input data
 	int lengthOfFile;
 
+	//make sure the file exists and is valid
 	if ( file != NULL ){
 
+		//first go to the end of the file and find out how many characters were counted
 		fseek(file, 0, SEEK_END);
+		//store number of charactes found.
 		lengthOfFile = ftell(file);
 
+		//allocate a character arry based on how many characters were found
+		//we need to + 1 because we need room for the null terminator character
 		line = (char *) malloc(lengthOfFile* sizeof(char) + 1 ) ;
 
+		//go back to the beginning of the input file
 		fseek (file , 0 , SEEK_SET);
 
+		//
 		char current_line[lengthOfFile+1];
 		while (fgets(current_line, sizeof(current_line), file)) {
 	        line = current_line;
 	    }
 		fclose ( file );
 	}
+	//error message if file does not exist. 
 	else{
 		printf ("File could not be opened.\r\n");
-		printf ("File may not exist or incorrectly named.\r\n");
+		printf ("File may not exist or the command line arugment is incorrectly named.\r\n");
+		//exit with 1 because exit(1) indicates that there were an error
+		exit(1);
    }
-
+   	// printf ("Character Count = %d.\r\n", lengthOfFile);
    	char answer[lengthOfFile];
 
 	char *dev_line;
@@ -60,15 +76,16 @@ int main ( int argc, char *argv[] )
 
 	cudaMemcpy(dev_line, line, size, cudaMemcpyHostToDevice);
 
-	convert2nums<<<1, 1>>>(dev_line,dev_answer);
+	decrement<<< 1, lengthOfFile >>>(dev_line, dev_answer);
 
 	cudaThreadSynchronize();
 
 	cudaMemcpy(answer, dev_answer, size, cudaMemcpyDeviceToHost);
 	
+    cudaFree(dev_line);
     cudaFree(dev_answer);
 
-	printf("Decoded Message is: \r\n%s",answer);
+	printf("Decoded --- Message is: \r\n%s",answer);
 
 	exit (0);
 }
--- makefile
+++ makefile
@@ -5,4 +5,4 @@ all: DecodeEmail2FULP
 DecodeEmail2FULP:      DecodeEmail2FULP.cu
 	$(GCC) -o DecodeEmail2FULP DecodeEmail2FULP.cu
 clean:
-	$(RM)  -f DecodeEmail2FULP DecodeEmail2FULP.o
+	$(RM)  DecodeEmail2FULP DecodeEmail2FULP.o
